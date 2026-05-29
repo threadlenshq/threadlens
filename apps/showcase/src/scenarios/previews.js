2b0175160084ed7ba130c853f194198210fbe90d
mrth2 <mr.th2.90@gmail.com>
2026-05-29 15:30:11 +0700

    feat(showcase): add scenario manifest and preview registry (Task 5)

---
+++ apps/showcase/src/scenarios/previews.js
@@ -0,0 +1,11 @@
+import EmptySetupPreview from '../previews/EmptySetupPreview.svelte';
+import FindingReviewPreview from '../previews/FindingReviewPreview.svelte';
+import ModalDecisionPreview from '../previews/ModalDecisionPreview.svelte';
+import WorkspaceContextPreview from '../previews/WorkspaceContextPreview.svelte';
+
+export const previewRegistry = {
+  EmptySetupPreview,
+  FindingReviewPreview,
+  ModalDecisionPreview,
+  WorkspaceContextPreview,
+};
